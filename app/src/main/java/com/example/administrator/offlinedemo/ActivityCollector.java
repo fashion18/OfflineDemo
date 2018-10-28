e991994451d20074b8e5843895c69c94a6550eb9
fashion18 <127913868@qq.com>
2018-10-28 15:29:31 +0800

    First commit

---
+++ app/src/main/java/com/example/administrator/offlinedemo/ActivityCollector.java
@@ -0,0 +1,27 @@
+package com.example.administrator.offlinedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+    public static void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+    public static void removeActivity(Activity activity) {
+        activities.remove(activity);
+    }
+    public static void finishAll() {
+        for (Activity activity : activities) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+        activities.clear();
+    }
+}
+
